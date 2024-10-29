dc9b39253e74d81a10cb6b02f5bc2ad033928c0a
Dodi Yulian <cdoditri@gmail.com>
2024-10-29 10:05:11 +0700

    add island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cyrdastroblog.netlify.app"
+  site: "https://cyrdastroblog.netlify.app",
+  integrations: [preact()]
 });
